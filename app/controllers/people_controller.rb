5155dba8cdbff742bb080f6d7afc1a8c6cf7b5d1
Kimler (KC) Corey <kimler@gmail.com>
2013-06-10 20:52:06 -0400

    remove additional json from people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,7 +6,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @people }
     end
   end
 
@@ -17,7 +16,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @person }
     end
   end
 
@@ -28,7 +26,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @person }
     end
   end
 
@@ -59,10 +56,8 @@ class PeopleController < ApplicationController
     respond_to do |format|
       if @person.update_attributes(params[:person])
         format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -75,7 +70,6 @@ class PeopleController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to people_url }
-      format.json { head :no_content }
     end
   end
 end
