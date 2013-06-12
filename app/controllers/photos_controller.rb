60ae090a791c989019471b83c1ecb53bf1560f1b
Kimler (KC) Corey <kimler@gmail.com>
2013-06-11 20:08:34 -0400

    removed extra photo code

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,70 +6,38 @@ class PhotosController < ApplicationController
   
   def index
     @photos = apply_scopes(Photo).all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @photos }
-    end
   end
 
-  # GET /photos/1
-  # GET /photos/1.json
   def show
     @photo = Photo.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @photo }
-    end
   end
 
-  # GET /photos/new
-  # GET /photos/new.json
   def new
     @photo = Photo.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @photo }
-    end
   end
 
-  # GET /photos/1/edit
   def edit
     @photo = Photo.find(params[:id])
   end
 
-  # POST /photos
-  # POST /photos.json
   def create
     @photo = Photo.new(params[:photo])
 
-    respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
-        format.json { render json: @photo, status: :created, location: @photo }
+        redirect_to @photo, notice: 'Photo was successfully created.' 
       else
-        format.html { render action: "new" }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
+        render action: "new" 
       end
-    end
   end
 
-  # PUT /photos/1
-  # PUT /photos/1.json
   def update
     @photo = Photo.find(params[:id])
 
-    respond_to do |format|
       if @photo.update_attributes(params[:photo])
-        format.html { redirect_to @photo, notice: 'Photo was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @photo, notice: 'Photo was successfully updated.' 
       else
-        format.html { render action: "edit" }
-        format.json { render json: @photo.errors, status: :unprocessable_entity }
+        render action: "edit" 
       end
-    end
   end
 
   # DELETE /photos/1
