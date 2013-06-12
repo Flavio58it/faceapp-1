5dd67c8e93903fd5caa1da29b65851b0f3127faa
Kimler (KC) Corey <kimler@gmail.com>
2013-06-11 21:05:52 -0400

    improved questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,96 +1,46 @@
 class QuestionsController < ApplicationController
-  # GET /questions
-  # GET /questions.json
+
   def index
     @questions = Question.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @questions }
-    end
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
   def show
-     @question = Question.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @question }
-    end
+    @question = Question.find(params[:id])
   end
-
 
   def ask
-    
     @random_question = Question.all.sample(1)
     @random_photo = Photo.all.sample(1)
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @question }
-    end
   end
 
-
-  # GET /questions/new
-  # GET /questions/new.json
   def new
     @question = Question.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @question }
-    end
   end
 
-  # GET /questions/1/edit
   def edit
     @question = Question.find(params[:id])
   end
 
-  # POST /questions
-  # POST /questions.json
   def create
     @question = Question.new(params[:question])
 
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
-        format.json { render json: @question, status: :created, location: @question }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to(@question, notice: 'Question was successfully created.') if @question.save    
+    render action: "new" unless @question.save 
   end
 
-  # PUT /questions/1
-  # PUT /questions/1.json
   def update
-    @question = Question.find(params[:id])
 
-    respond_to do |format|
       if @question.update_attributes(params[:question])
-        format.html { redirect_to @question, notice: 'Question was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to(@question, notice: 'Question was successfully updated.') 
       else
-        format.html { render action: "edit" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
+        render action: "edit" 
       end
-    end
   end
 
-  # DELETE /questions/1
-  # DELETE /questions/1.json
   def destroy
     @question = Question.find(params[:id])
-    @question.destroy
 
-    respond_to do |format|
-      format.html { redirect_to questions_url }
-      format.json { head :no_content }
-    end
+    @question.destroy
+    redirect_to questions_url
   end
 end
